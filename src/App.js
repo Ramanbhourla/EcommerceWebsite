091efb6fe5ef22cd1085d6274d7f5c193e17d079
Ramandeep Singh <rmnbhourla0004@gmail.com>
2022-02-19 13:06:34 +0530

    miniproject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BsFillGeoAltFill } from "react-icons/bs";
 
 function App() {
+  const [Search, setSearch] = useState("Ludhiana");
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    const fetchapi = async () => {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q= ${Search}&units=metric&appid=d870a1b4eb6d489ee5afd463403c7e03`
+      );
+      const apidata = await response.json();
+      setLocation(apidata.main);
+    };
+    fetchapi();
+  }, [Search]);
+
+  // console.log(location)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="header"> Weather App</h1>
+      <div className="WeatherBox">
+        <input
+          placeholder="Search here..."
+          className="input"
+          onChange={(e) => setSearch(e.target.value.toLocaleUpperCase())}
+        ></input>
+
+        {!location ? (
+          <div className="noData">No Data Found</div>
+        ) : (
+          <div>
+            <div className="cityname">
+              <BsFillGeoAltFill color="cadetblue" size="26px" />
+              <span className="cityname">{Search}</span>
+            </div>
+
+            <div className="temp"> Temprature : {location.temp}Cel.</div>
+            <div className="minimexi">
+              {" "}
+              <p>Maxi Temp : {location.temp_max}</p>
+              <p>Mini Temp : {location.temp_min}</p>
+              <div> Humidity : {location.humidity}</div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
